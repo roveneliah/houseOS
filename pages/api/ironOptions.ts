c66de75128ee7a509564133b5c0f93eef20119a7
Eli <ehanover21@gmail.com>
2022-06-30 08:46:32 +0800

    Update ironOptions.ts

--- pages/api/ironOptions.ts
+++ pages/api/ironOptions.ts
@@ -1,6 +1,6 @@
 export default {
   cookieName: "siwe",
-  password: process.env.NEXT_PUBLIC_ironOptionsPW,
+  password: process.env.NEXT_PUBLIC_ironOptionsPW || "", // TODO:
   cookieOptions: {
     secure: process.env.NODE_ENV === "production",
   },
