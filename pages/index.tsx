bb0e486720367660e9851fdf48a35cfd3a8ec932
Eli <ehanover21@gmail.com>
2022-07-20 16:52:47 -0400

    remove dashboard view in favor of fixed search

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,16 +9,16 @@ import Layout from "../components/Layout";
 
 const Home: NextPage = () => {
   const { data } = useBalance({ addressOrName: dao.treasury });
-  const ethBalance = Number(data?.formatted).toFixed(2);
   const proposals = useGetProposals(snapshotSpace);
+  const ethBalance = Number(data?.formatted).toFixed(2);
   const liveProposals = length(
     proposals.filter(pipe(prop("state"), equals("active")))
   );
   const users = useGetUsers();
 
   return (
-    <Layout noOpacity={true}>
-      <div className="flex w-full flex-row justify-center pt-32">
+    <Layout noOpacity={true} fixedOpen={true}>
+      {/* <div className="flex w-full flex-row justify-center pt-32">
         <div className="flex w-2/3 flex-col space-y-4">
           <div className="flex flex-row space-x-4">
             <div className="flex basis-2/3 flex-col space-y-4 rounded-lg border p-12">
@@ -41,7 +41,7 @@ const Home: NextPage = () => {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </Layout>
   );
 };
