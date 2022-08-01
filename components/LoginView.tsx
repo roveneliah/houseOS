53706dcbd053d0dcdc177de5a675ec21dda9beb3
Eli <ehanover21@gmail.com>
2022-08-01 15:12:12 -0400

    Update LoginView.tsx

--- components/LoginView.tsx
+++ components/LoginView.tsx
@@ -1,7 +1,17 @@
+import { dao } from "@/config";
+
 export default function LoginView() {
   return (
-    <div className="w-full px-72 pt-36">
-      <p className="text-6xl font-bold">Please log in!</p>
+    <div className="flex w-full flex-row justify-center px-72 pt-36">
+      <div className="bg-primary-content flex w-[40vw] flex-col space-y-4 rounded-lg p-8 py-8">
+        <p className="text-neutral text-2xl font-bold">
+          Please log in to view your profile.
+        </p>
+        <p className="text-neutral/75 text-md font-bold">
+          Sign in to connect with {dao.memberName}'s, participate in governance,
+          and use other widgets coming soon.
+        </p>
+      </div>
     </div>
   );
 }
