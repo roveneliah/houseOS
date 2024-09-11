5903c1edca8cc12bbbbae01c4af56bf54c7c650c
Eli <ehanover21@gmail.com>
2024-09-10 18:00:11 -0700

    Update DesktopIconsBasic.tsx

--- components/home/DesktopIconsBasic.tsx
+++ components/home/DesktopIconsBasic.tsx
@@ -11,6 +11,17 @@ export function DesktopIconsBasic() {
   const toggleSearch = () => dispatch(toggle({ windowName: "search" }));
   return (
     <div className="absolute bottom-12 w-full flex-row justify-between px-16 sm:left-10 sm:top-24 sm:flex sm:w-fit sm:flex-col  sm:justify-start sm:space-x-0 sm:space-y-8 sm:px-0">
+      <a target="_blank" href="https://www.krausehouse.club/media/">
+        <div className="flex flex-col items-center space-y-1">
+          <Image
+            alt="Updates"
+            src="/desktop-icons/Doc.png"
+            width={40}
+            height={50}
+          />
+          <p className="font-mono">Updates</p>
+        </div>
+      </a>
       <a target="_blank" href="https://discord.gg/wAjEq3CM">
         <div className="flex flex-col items-center space-y-1">
           <Image
@@ -33,15 +44,20 @@ export function DesktopIconsBasic() {
           <p className="font-mono">Search</p>
         </div>
       </button> */}
-      {/* <a
+      <a
         target="_blank"
         href="https://krausehousework.notion.site/Krause-House-Contributor-Dashboard-a00860761dd4486792aed12cc8187ce2"
       >
         <div className="flex flex-col items-center space-y-1">
-          <Image src="/desktop-icons/Files.png" width={50} height={50} />
+          <Image
+            alt="FAQ"
+            src="/desktop-icons/Files.png"
+            width={50}
+            height={50}
+          />
           <p className="font-mono">FAQ</p>
         </div>
-      </a> */}
+      </a>
       {/* <a
         target="_blank"
         href="https://krausehousework.notion.site/Krause-House-Contributor-Dashboard-a00860761dd4486792aed12cc8187ce2"
@@ -60,17 +76,6 @@ export function DesktopIconsBasic() {
             height={50}
           />
           <p className="font-mono">Voting</p>
-        </div>
-      </a>
-      <a target="_blank" href="https://www.ballhogs.club/">
-        <div className="flex flex-col items-center space-y-1">
-          <Image
-            alt="FAQ"
-            src="/desktop-icons/Doc.png"
-            width={40}
-            height={50}
-          />
-          <p className="font-mono">FAQ</p>
         </div>
       </a>
     </div>
